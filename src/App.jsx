533ff856166c674c0ca906b2c1a8b9353e2f3f25
juanpablo1978 <jpandrujovich@gmail.com>
2025-09-03 08:21:58 -0300

    instalacion de dependencias

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main className='min-h-screen flex justify-center items-center bg-black text-white'>App</main>
+  )
+}
+
+export default App
